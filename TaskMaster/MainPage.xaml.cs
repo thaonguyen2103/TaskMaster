72b2ee1e131cbd3d6ff42006dcc15f9ce14c222e
thaonguyen2103 <thaonguyenhoang2103@gmail.com>
2024-10-31 19:13:35 +0700

    update UI

---
+++ TaskMaster/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.UI.Text;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TaskMaster
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Xử lý khi người dùng nhấn vào My Project
+        public void showTaskContainer()
+        {
+            Mytask_Container.Visibility = Visibility.Visible;
+            Myproject_Container.Visibility = Visibility.Collapsed;
+        }
+        public void showProjectContainer()
+        {
+            Myproject_Container.Visibility = Visibility.Visible;
+            Mytask_Container.Visibility = Visibility.Collapsed;
+        }
+        // Xử lý khi người dùng nhấn vào My Project
+        private void MyProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            showProjectContainer();
+        }
+        private void MyTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            showTaskContainer();
+        }
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            AccountPopup.IsOpen = !AccountPopup.IsOpen;
+
+        }
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            AccountPopup.IsOpen = false;
+            Frame.Navigate(typeof(LoginPage));
+        }
+        private void MyAccountButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MyAccountPage));
+        }
+        private void NotificationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CreateProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            CreateProjectField.IsOpen = true;
+            JoinProjectField.IsOpen = false;
+
+        }
+        private void CreateProjectButton_QuitClick(object sender, RoutedEventArgs e)
+        {
+            CreateProjectField.IsOpen = false;
+
+        }
+        //Tạo dự án mới
+        private void SaveNewProject_Click(object sender, RoutedEventArgs e)
+        {
+            CreateProjectField.IsOpen = false;
+            Button newProject = new Button
+            {
+                Content = CreateProject_Name.Text,
+                Style = (Style)Application.Current.Resources["CustomProjectIconStyle"],
+
+            };
+            newProject.Click += ProjectView_Click;
+            ProjectList.Children.Add(newProject);
+
+        }
+        private void ProjectView_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Project));
+        }
+
+        private void JoinProjectButton_Click(Object sender, RoutedEventArgs e)
+        {
+            JoinProjectField.IsOpen = true;
+            CreateProjectField.IsOpen = false;
+        }
+        private void JoinProjectButton_QuitClick(object sender, RoutedEventArgs e)
+        {
+            JoinProjectField.IsOpen = false;
+        }
+        //Tham gia một dự án có sẵn
+        private void SaveExistingProject_Click(object sender, RoutedEventArgs e)
+        {
+            JoinProjectField.IsOpen = false;
+            Button existingProject = new Button
+            {
+                Content = JoinProject_Code.Text,
+                Style = (Style)Application.Current.Resources["CustomProjectIconStyle"],
+                Background = new SolidColorBrush(Microsoft.UI.Colors.Purple)
+            };
+            existingProject.Click += ProjectView_Click;
+            ProjectList.Children.Add(existingProject);
+
+        }
+        private void Addnewtask_Click(object sender, RoutedEventArgs e)
+        {
+            Grid taskGrid = new Grid
+            {
+                Style = (Style)Application.Current.Resources["CustomTaskGridStyle"]
+            };
+            taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
+            taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            CheckBox checkbox = new CheckBox
+            {
+                Content = "Do something"
+            };
+            Grid.SetColumn(checkbox, 0);
+            taskGrid.Children.Add(checkbox);
+
+            TextBlock sourceTask = new TextBlock
+            {
+                Text = "Project B"
+            };
+            Grid.SetColumn(sourceTask, 1);
+            taskGrid.Children.Add(sourceTask);
+
+            TextBlock start = new TextBlock
+            {
+                Text = "02/02/2024"
+            };
+            Grid.SetColumn(start, 2);
+            taskGrid.Children.Add(start);
+
+            TextBlock due = new TextBlock
+            {
+                Text = "20/02/2024"
+            };
+            Grid.SetColumn(due, 3);
+            taskGrid.Children.Add(due);
+
+            TaskList.Children.Add(taskGrid);
+        }
+    }
+}
---
+++ TaskMaster/Project.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Media.Imaging;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace TaskMaster
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Project : Page
+    {
+        public Project()
+        {
+            this.InitializeComponent();
+        }
+        private void ProfileButton_Click(object sender, RoutedEventArgs e)
+        {
+            AccountPopup.IsOpen = !AccountPopup.IsOpen;
+
+        }
+        private void LogOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            AccountPopup.IsOpen = false;
+            Frame.Navigate(typeof(LoginPage));
+        }
+        private void NotificationButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void MyTaskButton_Click(Object sender, RoutedEventArgs e) {
+            Frame.Navigate(typeof(MainPage));
+            MainPage mainPage = (MainPage)Frame.Content;
+            mainPage.showTaskContainer();
+        }
+        private void MyProjectButton_Click(Object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+            MainPage mainPage = (MainPage)Frame.Content;
+            mainPage.showProjectContainer();
+        }
+        bool isPinned = false; 
+        private void Pin_Click(object sender, TappedRoutedEventArgs e)
+        {
+            if (!isPinned)
+            {
+                PinBtn.Source = new BitmapImage(new Uri("ms-appx:///Assets/Image/pinned.png"));
+                isPinned = true;
+            }
+            else
+            {
+                PinBtn.Source = new BitmapImage(new Uri("ms-appx:///Assets/Image/star.png"));
+                isPinned = false;
+            }
+        }
+        private Popup list_more; 
+        private void List_more_open(object sender, TappedRoutedEventArgs e)
+        {
+            if (list_more == null)
+            {
+                list_more = new Popup
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    IsOpen = false 
+                };
+                StackPanel stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Vertical,
+                    Padding = new Thickness(5),
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.White),
+                    BorderBrush = new SolidColorBrush(Microsoft.UI.Colors.Black),
+                    BorderThickness = new Thickness(1),
+                    CornerRadius = new CornerRadius(8)
+                };
+
+                Button deleteButton = new Button
+                {
+                    Content = "Delete",
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.Transparent),
+                    FontSize = 13
+                };
+                stackPanel.Children.Add(deleteButton);
+
+                Button renameButton = new Button
+                {
+                    Content = "Rename",
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.Transparent),
+                    FontSize = 13
+                };
+                stackPanel.Children.Add(renameButton);
+
+                list_more.Child = stackPanel;
+            }
+            var image = sender as Image;
+            if (image != null)
+            {
+                list_more.XamlRoot = this.XamlRoot;
+                if (list_more.IsOpen)
+                {
+                    list_more.IsOpen = false;
+                }
+                else
+                {
+                    var position = image.TransformToVisual(this).TransformPoint(new Point(0, 0));
+                    list_more.HorizontalOffset = position.X + image.ActualWidth;
+                    list_more.VerticalOffset = position.Y;
+                    list_more.IsOpen = true;
+                }
+            }
+        }
+        private void addTask_Click (object  sender, RoutedEventArgs e)
+        {
+            CreateTaskField.IsOpen = true;
+        }
+        private Popup task_more;
+        private void Task_more_open(object sender, TappedRoutedEventArgs e)
+        {
+            if (task_more == null)
+            {
+                task_more = new Popup
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    IsOpen = false 
+                };
+                StackPanel stackPanel = new StackPanel
+                {
+                    Orientation = Orientation.Vertical,
+                    Padding = new Thickness(5),
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.White),
+                    BorderBrush = new SolidColorBrush(Microsoft.UI.Colors.Black),
+                    BorderThickness = new Thickness(1),
+                    CornerRadius = new CornerRadius(8)
+                };
+                Button deleteButton = new Button
+                {
+                    Content = "Delete",
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.Transparent),
+                    FontSize = 13
+                };
+                stackPanel.Children.Add(deleteButton);
+                Button renameButton = new Button
+                {
+                    Content = "Rename",
+                    Background = new SolidColorBrush(Microsoft.UI.Colors.Transparent),
+                    FontSize = 13
+                };
+                stackPanel.Children.Add(renameButton);
+
+                task_more.Child = stackPanel;
+            }
+            var image = sender as Image;
+            if (image != null)
+            {
+                task_more.XamlRoot = this.XamlRoot;
+
+                if (task_more.IsOpen)
+                {
+                    task_more.IsOpen = false;
+                }
+                else
+                {
+                    var position = image.TransformToVisual(this).TransformPoint(new Point(0, 0));
+                    task_more.HorizontalOffset = position.X + image.ActualWidth;
+                    task_more.VerticalOffset = position.Y;
+                    task_more.IsOpen = true;
+                }
+            }
+        }
+
+        private void addList_Click(object sender, RoutedEventArgs e)
+        {
+            NewList_Btn.Visibility = Visibility.Collapsed;
+            AddNewList_Btn.Visibility = Visibility.Visible;
+            EnterNewNameList.Visibility = Visibility.Visible;
+            Cancel_AddNewList_Btn.Visibility = Visibility.Visible;
+        }
+        private void NewList_Cancel(object sender, RoutedEventArgs e)
+        {
+            NewList_Btn.Visibility = Visibility.Visible;
+            AddNewList_Btn.Visibility = Visibility.Collapsed;
+            EnterNewNameList.Visibility = Visibility.Collapsed;
+            Cancel_AddNewList_Btn.Visibility = Visibility.Collapsed;
+        }
+        private void NewList_Add(object sender, RoutedEventArgs e)
+        {
+            //StackPanel newList = new StackPanel
+            //{
+            //    Style = (Style)Application.Current.Resources["CustomListFrameStyle"]
+            //};
+            //StackPanel list_header = new StackPanel
+            //{
+            //    Orientation = Orientation.Horizontal,
+            //};
+            //TextBlock list_header_name = new TextBlock
+            //{
+            //    Text = EnterNewNameList.Text,
+            //    FontSize = 16,
+
+            //};
+            //Image list_header_more = new Image()
+            //{
+            //    HorizontalAlignment = HorizontalAlignment.Right,
+            //    Source = new BitmapImage(new Uri("ms-appx:///Assets/Image/moreList.png")),
+            //    Width = 20,
+
+            //};
+            //list_header_more.PointerPressed += list_more_Click;
+            //list_header.Children.Add(list_header_name);
+            //list_header.Children.Add(list_header_more);
+            //newList.Children.Add(list_header);
+            //List_Container.Children.Add(newList);
+            //AddNewList_Btn.Visibility = Visibility.Collapsed;
+            //EnterNewNameList.Visibility = Visibility.Collapsed;
+            //NewList_Btn.Visibility = Visibility.Visible;
+        }
+        private void list_more_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
